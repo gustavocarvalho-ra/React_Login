94eef08e87856acc8e83597cec02abfcd1f268b8
GustavoCR021 <gustavoc1922@gmail.com>
2024-02-16 21:46:49 -0300

    at2

--- reactlogin/src/components/LoginForm/LoginForm.jsx
+++ reactlogin/src/components/LoginForm/LoginForm.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import './LoginForm.css'
+
 
 const LoginForm = () => {
   return (
-    <div>LoginForm</div>
+    <div className="container">
+      <form action="">
+        <h1>Login</h1>
+        <div className="input-box">
+          <input type="text" placeholder='Nome de Usuário' required />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder='Senha' required />
+        </div>
+
+        <div className="esqueceu-senha">
+          <label><input type="checkbox" />Salvar a senha</label>
+          <a href="#">Esqueci a senha</a>
+        </div>
+
+        <button type='submit'>Login</button>
+
+        <div className="register-link">
+          <p>Não possui conta? <a href="#">Registrar</a></p>
+        </div>
+      </form>
+    </div>
   )
 }
 
