0d5752c288f9d0273614e422855fe28c997f3693
GustavoCR021 <gustavoc1922@gmail.com>
2024-02-16 21:29:20 -0300

    at1

---
+++ reactlogin/src/App.js
@@ -0,0 +1,13 @@
+import LoginForm from './components/LoginForm/LoginForm';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
